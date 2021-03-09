0c80882ba53fc7c4c9095e15fcaa4fb57c0cb33a
revanthkumaar <iamrevanthkumar@gmail.com>
2021-03-09 11:49:20 +0530

    kajshdkh
    askhjdkasj

--- App.js
+++ App.js
@@ -1,51 +1,31 @@
-import React, {useState} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  //setting the initial state
   const [enteredGoal, setEnteredGoal] = useState('');
-  const [courseGoals, setCourseGoals] = useState([]); //setting initial value as empty array
+  const [courseGoals, setCourseGoals] = useState([]);
 
   const goalInputHandler = (enteredText) => {
     setEnteredGoal(enteredText);
   };
 
   const addGoalHandler = () => {
-    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
   };
-
-  // ['eat bf','join class','sleep','sleep in the class']
 
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput
-          placeholder="Goals/Tasks"
+          placeholder="Course Goal"
           style={styles.input}
-          onChangeText={goalInputHandler()}
+          onChangeText={goalInputHandler}
           value={enteredGoal}
         />
         <Button title="ADD" onPress={addGoalHandler} />
       </View>
       <View>
-        {courseGoals.map((goal) => (
-          <Text>{goal}</Text>
-        ))}
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
       </View>
     </View>
   );
@@ -53,12 +33,17 @@ export default function App() {
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 50,
+    padding: 50
   },
   inputContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center',
+    alignItems: 'center'
   },
-  input: {width: '80%', borderColor: 'black', borderWidth: 1, padding: 10},
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  }
 });
