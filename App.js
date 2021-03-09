464e14346f687fdff024b21f267d970a8317054e
revanthkumaar <iamrevanthkumar@gmail.com>
2021-03-09 10:15:02 +0530

    sadasd
    asdad

--- App.js
+++ App.js
@@ -54,36 +54,24 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
+    <View style={{ padding: 50, flexDirection:'row-reverse',width:'80%',height:300,justifyContent:'space-between',alignItems:'stretch'}}>
       <View
         style={{
-          backgroundColor: 'red',
-          width: 100,
-          height: 100,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
+          backgroundColor: 'red'
+          }}
       >
         <Text>1</Text>
       </View>
       <View
         style={{
-          backgroundColor: 'blue',
-          width: 100,
-          height: 100,
-          justifyContent: 'center',
-          alignItems: 'center'
+          backgroundColor: 'blue'
         }}
       >
         <Text>2</Text>
       </View>
       <View
         style={{
-          backgroundColor: 'green',
-          width: 100,
-          height: 100,
-          justifyContent: 'center',
-          alignItems: 'center'
+          backgroundColor: 'green'
         }}
       >
         <Text>3</Text>
